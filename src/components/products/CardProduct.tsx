aec3dc29f243084c7af90400e03f5f1f489aab0d
Jhon <eduardcamayo13@gmail.com>
2024-06-28 20:13:50 -0500

    Agregando estilos

--- src/components/products/CardProduct.tsx
+++ src/components/products/CardProduct.tsx
@@ -1,20 +1,26 @@
-import Link from 'next/link'
+import Link from "next/link";
 interface InterfaceProduct {
-    img: string
-    nameProduct: string
-    price: string
-    href: string
+    img: string;
+    nameProduct: string;
+    price: string;
+    href: string;
 }
 
-const CardProduct = ({img, nameProduct, price, href}: InterfaceProduct) => {
-  return (
-    <div>
-        <img src={img} alt={nameProduct} />
-        <h4>{nameProduct}</h4>
-        <h5>{price}</h5>
-        <Link href={href}>Ver producto</Link>
-    </div>
-  )
-}
+const CardProduct = ({ img, nameProduct, price, href }: InterfaceProduct) => {
+    return (
+        <div className="w-full">
+            <img
+                className="object-cover h-[200px] w-full"
+                src={img}
+                alt={nameProduct}
+            />
+            <div className="flex gap-1 flex-col p-2">
+                <h4 className="text-sm">{nameProduct}</h4>
+                <h5 className="font-semibold">$ {price}</h5>
+                <Link className="text-sm text-[#2A7AE4] font-bold" href={href}>Ver producto</Link>
+            </div>
+        </div>
+    );
+};
 
-export default CardProduct
+export default CardProduct;
