0dec0b4b4cb0c0a4080f2993d2a8fbb9bceea97c
Jhon <eduardcamayo13@gmail.com>
2024-06-29 20:55:46 -0500

    Agregando provider en el layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,28 +3,28 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
-import { ProductsProvaider } from "@/context/productsContext";
+import ProviderProducts from "@/context/ProviderProducts";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Alura Geek",
-  description:
-    "Descubre lo mejor en productos geek en Alura Geek. Ofrecemos una amplia gama de artículos como figuras de acción, cómics, ropa temática, gadgets y más. Compra con confianza y disfruta de envío rápido y seguro. ¡Visítanos y encuentra tus productos favoritos hoy!",
+    title: "Alura Geek",
+    description:
+        "Descubre lo mejor en productos geek en Alura Geek. Ofrecemos una amplia gama de artículos como figuras de acción, cómics, ropa temática, gadgets y más. Compra con confianza y disfruta de envío rápido y seguro. ¡Visítanos y encuentra tus productos favoritos hoy!",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Header />
+                <ProviderProducts>{children}</ProviderProducts>
+                <Footer />
+            </body>
+        </html>
+    );
 }
