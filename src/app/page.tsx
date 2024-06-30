82bb393d5f1ac62a27be3a96eeaa3c0d38ca95e4
Jhon <eduardcamayo13@gmail.com>
2024-06-29 20:57:36 -0500

    Eliminando el productsprovider de la página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-// pages/index.tsx (Home component)
 import Banner from "@/components/Banner";
 import SectionCategory from "@/components/products/SectionCategory";
-import { ProductsProvider } from "@/context/productsContext";
 
 const Home = () => {
     return (
-        <ProductsProvider>
-            <div>
-                <Banner />
-                <SectionCategory />
-            </div>
-        </ProductsProvider>
+        <div>
+            <Banner />
+            <SectionCategory />
+        </div>
     );
 };
 
